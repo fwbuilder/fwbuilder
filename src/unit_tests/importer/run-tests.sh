1c01b614ba04d7df454d366125435667c2b7c5d8
Vadim Kurland <vadim@netcitadel.com>
2008-02-13 04:27:06 +0000

    adjusted unit testst for policy importer

--- src/unit_tests/importer/run-tests.sh
+++ src/unit_tests/importer/run-tests.sh
@@ -10,9 +10,9 @@ cmp test1.result test_data/test1.result &&  \
 cmp test1.stderr test_data/test1.stderr
 
 echo "iptables" && \
-./importer_test iptables < test_data/iptables_test1.conf > test1.result 2> test1.stderr
-cmp test1.result test_data/iptables_test1.result &&  \
-cmp test1.stderr test_data/iptables_test1.stderr &&  \
+./importer_test iptables < test_data/iptables_test1.conf > iptables_test1.result 2> iptables_test1.stderr
+cmp iptables_test1.result test_data/iptables_test1.result &&  \
+cmp iptables_test1.stderr test_data/iptables_test1.stderr &&  \
 
 echo PASS || echo FAIL
 
