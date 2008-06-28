72f4ed435716eb54c89e21207bfc87c8880a21db
Vadim Kurland <vadim@netcitadel.com>
2008-06-28 09:10:59 +0000

    fix in getHostByAddr

--- src/fwbuilder/dns.cpp
+++ src/fwbuilder/dns.cpp
@@ -237,7 +237,7 @@ HostEnt DNS::getHostByAddr(const InetAddr &addr, int retries_, int timeout_) thr
     char host[NS_MAXDNAME];
     string addr_c = addr.toString();
     string octets[4];
-    size_type p1, p2;
+    string::size_type p1, p2;
     int idx = 0;
     p1 = p2 = 0;
     while ( (p2=addr_c.find_first_of('.', p1))!=string::npos )
@@ -246,8 +246,8 @@ HostEnt DNS::getHostByAddr(const InetAddr &addr, int retries_, int timeout_) thr
         p1 = p2+1;
         idx++;
     }
-    sprintf(host, "%u.%u.%u.%u.IN-ADDR.ARPA.",
-            octets[3], octets[2], octets[1], octets[0]);
+    sprintf(host, "%s.%s.%s.%s.IN-ADDR.ARPA.",
+            octets[3].c_str(), octets[2].c_str(), octets[1].c_str(), octets[0].c_str());
 
     u_char buf[PACKETSZ];
     int n = res_nmkquery(&res, ns_o_query, host, ns_c_in, ns_t_ptr, NULL, 0, NULL,
