60077b1095fec4fd7e5b1a5d812c905a5a75e118
SVN User <svn@netcitadel.com>
2008-07-13 01:39:06 +0000

    fixed a bug where "&" was used instead of "&&" Added copyright, license and author comment at the top

--- src/gui/ObjectListViewItem.cpp
+++ src/gui/ObjectListViewItem.cpp
@@ -1,10 +1,38 @@
+/*
+
+                          Firewall Builder
+
+                 Copyright (C) 2008 NetCitadel, LLC
+
+  Author:  alek@codeminders.com
+
+  $Id$
+
+  This program is free software which we release under the GNU General Public
+  License. You may redistribute and/or modify this program under the terms
+  of that license as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  To get a copy of the GNU General Public License, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+
 #include "ObjectListViewItem.h"
 
 #include "fwbuilder/TCPUDPService.h"
 #include "fwbuilder/FWObject.h"
 #include "fwbuilder/IPService.h"
 #include "fwbuilder/ICMPService.h"
+
 #include <QTreeWidget>
+
 using namespace libfwbuilder;
 
 bool ObjectListViewItem::operator< ( const QTreeWidgetItem & other ) const
@@ -51,7 +79,7 @@ bool ObjectListViewItem::operator< ( const QTreeWidgetItem & other ) const
                 int rpn = rip->getProtocolNumber();
                 return (lpn < rpn);
             }
-            if (ricmp != NULL & licmp != NULL)
+            if (ricmp != NULL && licmp != NULL)
             {
                 int lpn = licmp->getInt("code");
                 int rpn = ricmp->getInt("code");
