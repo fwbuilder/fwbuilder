01a9206030060a8ab92f1125072d127b7668eff5
SVN User <svn@netcitadel.com>
2008-07-14 23:24:43 +0000

    about dialog

--- src/gui/FWBAboutDialog.h
+++ src/gui/FWBAboutDialog.h
@@ -21,16 +21,18 @@ public:
         m_aboutDialog->setupUi(this);
         
         //it was the "init" method of AboutDialog_q:
-        m_aboutDialog->titleLbl->setText( QString("Firewall Builder v%1").arg(VERSION) );
-        m_aboutDialog->revLbl->setText( tr("Revision: %1 ( Build: %2 )").arg(RELEASE_NUM).arg(BUILD_NUM) );
+        m_aboutDialog->titleLbl->setText(
+            QString("Firewall Builder v%1").arg(VERSION) );
+        m_aboutDialog->revLbl->setText(
+            tr("Revision: %1 ( Build: %2 )").arg(RELEASE_NUM).arg(BUILD_NUM) );
     
-        m_aboutDialog->apiLbl->setText( tr("Using Firewall Builder API %1").arg(libfwbuilder::Constants::getLibraryVersion().c_str() ) );
+        m_aboutDialog->apiLbl->setText(
+            tr("Using Firewall Builder API %1").arg(
+                libfwbuilder::Constants::getLibraryVersion().c_str() ) );
 
 #ifdef ELC
-	// TODO(vadim): add UI element to print 'Registered' or 'Unregistered'
-	// to the About dialog.
-        //if (registered) reg->setText(tr("Registered"));
-        //else            reg->setText(tr("Unregistered"));
+        if (registered) m_aboutDialog->reg->setText(tr("Registered"));
+        else            m_aboutDialog->reg->setText(tr("Unregistered"));
 #endif
         
         setWindowTitle(QString("Firewall Builder: About..."));
