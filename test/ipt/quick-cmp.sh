483dab7051bf09df61e4abaee128ec14d28f7229
Vadim Kurland <vadim@netcitadel.com>
2008-10-21 02:49:41 +0000

    * Compiler.cpp (Compiler::_expand_addr_recursive): change in the algorithm used to decide which interfaces of the host or firewall object to use in a rule when this host or firewall object is found in source or destination.

--- test/ipt/quick-cmp.sh
+++ test/ipt/quick-cmp.sh
@@ -2,7 +2,7 @@
 
 
 XMLFILE=$1
-DIFFCMD="diff -C 1 -c -b -B -I \"#  Generated\" -I 'Activating ' -I '#  Firewall Builder  fwb_ipt v' -I 'Can not find file' -I '====' -I 'log '"
+DIFFCMD="diff -C 5 -c -b -B -I \"#  Generated\" -I 'Activating ' -I '#  Firewall Builder  fwb_ipt v' -I 'Can not find file' -I '====' -I 'log '"
 
 fwbedit list -f $XMLFILE -o /User/Firewalls -c -F%name% | sort | while read fwobj; do
   echo "$DIFFCMD ${fwobj}.fw.orig ${fwobj}.fw"
