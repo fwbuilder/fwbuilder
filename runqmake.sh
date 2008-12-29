2989fc779f182943067be2ddf795d02b00c7f0c0
Vadim Kurland <vadim@netcitadel.com>
2008-12-29 17:56:03 +0000

    avoid += shell operator, it does not work on freebsd

--- runqmake.sh
+++ runqmake.sh
@@ -3,8 +3,8 @@
 test -z "${QMAKE}" && QMAKE="qmake"
 
 C=""
-test -n "$QMAKESPEC" && C+="-spec $QMAKESPEC "
-test -n "$CCACHE" && C+='QMAKE_CXX="ccache	g++" '
+test -n "$QMAKESPEC" && C="$C -spec $QMAKESPEC "
+test -n "$CCACHE" && C="$C QMAKE_CXX=\"ccache	g++\" "
 
 echo "QTDIR=\"$QTDIR\""
 echo "Running qmake: $QMAKE $C"
@@ -14,22 +14,3 @@ IFS="
 "
 
 $QMAKE $C -recursive
-
-
-
-# test -n "$CCACHE" && C='QMAKE_CXX="ccache g++"'
-# test -n "$QMAKESPEC" && C="-spec $QMAKESPEC $C"
-
-# echo "QTDIR=$QTDIR"
-# echo "Running qmake: ${QMAKE} $C"
-
-# oIFS=$IFS
-# IFS=
-# ${QMAKE} $C
-# for d in src/ src/fwbuilder/ src/fwcompiler/ src/test/ \
-#          src/confscript/ etc/ doc/ migration/
-# do
-#   (cd $d; ${QMAKE} $C)
-# done
-# IFS=$oIFS
-
