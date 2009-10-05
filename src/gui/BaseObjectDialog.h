aed71f9ea9d647c2bc86d9db6cae0a26608ec285
Vadim Kurland <vadim@netcitadel.com>
2009-10-05 02:23:38 +0000

    fixing file src/gui/BaseObjectDialog.h

--- src/gui/BaseObjectDialog.h
+++ src/gui/BaseObjectDialog.h
@@ -6,7 +6,7 @@
 
   Author:  Vadim Kurland     vadim@fwbuilder.org
 
-  $Id: ObjectEditor.h 1537 2009-10-02 00:36:43Z vadim $
+  $Id$
 
   This program is free software which we release under the GNU General Public
   License. You may redistribute and/or modify this program under the terms
