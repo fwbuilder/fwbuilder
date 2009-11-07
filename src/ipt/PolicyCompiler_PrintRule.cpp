81af871dc623dce456c8e36beba71859cf40a952
Vadim Kurland <vadim@netcitadel.com>
2009-11-07 15:27:59 +0000

    fixed bug introduced in the previous build, missing -s in iptable commands built from AddressRange objects representing single address

--- src/ipt/PolicyCompiler_PrintRule.cpp
+++ src/ipt/PolicyCompiler_PrintRule.cpp
@@ -1106,7 +1106,7 @@ string PolicyCompiler_ipt::PrintRule::_printSrcAddr(RuleElement *rel,
             res += _printSingleObjectNegation(rel) + "--src-range ";
             res += range_start.toString() + "-" + range_end.toString() + " ";
         } else
-            res += range_start.toString() + " ";
+            res += "-s " + range_start.toString() + " ";
 
         return res;
     }
@@ -1128,7 +1128,7 @@ string PolicyCompiler_ipt::PrintRule::_printDstAddr(RuleElement *rel,
             res += _printSingleObjectNegation(rel) + "--dst-range ";
             res += range_start.toString() + "-" + range_end.toString() + " ";
         } else
-            res += range_start.toString() + " ";
+            res += "-s " + range_start.toString() + " ";
 
         return res;
     }
