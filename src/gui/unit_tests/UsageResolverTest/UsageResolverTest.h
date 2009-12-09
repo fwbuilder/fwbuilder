33641a4ff065489bd15e7ce577e3df287636e77e
Roman Bovsunivskiy <a2k@codeminders.com>
2009-12-09 20:27:31 +0000

    Fixed #812

--- src/gui/unit_tests/UsageResolverTest/UsageResolverTest.h
+++ src/gui/unit_tests/UsageResolverTest/UsageResolverTest.h
@@ -1,3 +1,28 @@
+/*
+
+                          Firewall Builder
+
+                 Copyright (C) 2009 NetCitadel, LLC
+
+  Author:  Roman Bovsunivkiy     a2k0001@gmail.com
+
+  $Id$
+
+  This program is free software which we release under the GNU General Public
+  License. You may redistribute and/or modify this program under the terms
+  of that license as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  To get a copy of the GNU General Public License, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
 #ifndef FWOBJECTDATABASETEST_H
 #define FWOBJECTDATABASETEST_H
 
