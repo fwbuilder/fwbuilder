1c4c12e181026b597f13b02331cd493d7438c332
Roman Bovsunivskiy <a2k@codeminders.com>
2010-02-06 14:28:31 +0000

    moved DNSTest for #1162

---
+++ src/fwbuilder/unit_tests/DNSTest/DNSTest.pro
@@ -0,0 +1,17 @@
+
+include(../../../../qmake.inc)
+
+QT -= core \
+    gui
+TARGET = DNSTest
+CONFIG += console
+CONFIG -= app_bundle
+TEMPLATE = app
+SOURCES += main.cpp \
+    DNSTest.cpp
+HEADERS += DNSTest.h
+INCLUDEPATH += ../../..
+LIBS += -lcppunit -lfwbuilder
+run.commands = echo "Running tests..." && ./${TARGET}
+run.depends = all
+QMAKE_EXTRA_TARGETS += run
---
+++ src/fwbuilder/unit_tests/DNSTest/main.cpp
@@ -0,0 +1,44 @@
+/*
+
+                          Firewall Builder
+
+                 Copyright (C) 2009 NetCitadel, LLC
+
+  Author:  Roman Bovsunivkiy     a2k0001@gmail.com
+
+  $Id$
+
+  This program is free software which we release under the GNU General Public
+  License. You may redistribute and/or modify this program under the terms
+  of that license as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  To get a copy of the GNU General Public License, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/CompilerOutputter.h>
+#include "DNSTest.h"
+#include <string>
+#include "FWObjectDatabase.h"
+
+int fwbdebug = 0;
+std::string platform;
+
+int main( int, char** argv)
+{
+    CppUnit::TextUi::TestRunner runner;
+    runner.addTest( DNSTest::suite() );
+    runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(),
+                                                         std::cerr ) );
+
+    runner.run();
+    return 0;
+}
