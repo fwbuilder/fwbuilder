6742d104d4834d9d62781728835d01eca7a38412
Roman Bovsunivskiy <a2k@codeminders.com>
2010-02-28 17:11:11 +0000

    stop building tests on fail

--- src/fwbuilder/unit_tests/unit_tests.sh
+++ src/fwbuilder/unit_tests/unit_tests.sh
@@ -6,6 +6,6 @@ for directory in `find . -maxdepth 1 -type d -regex '\./[A-Za-z0-9\-\_]*'`
 do
     cd $directory
     [ ! -e Makefile ] && qmake -spec $QMAKESPEC
-    $commands
+    $commands || exit 1
     cd -
 done;
