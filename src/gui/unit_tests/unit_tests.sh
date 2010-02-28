9fc62a9f8263840f97cca62a038e473c83566acc
Vadim Kurland <vadim@netcitadel.com>
2010-02-28 02:47:16 +0000

    * unit_tests.sh (commands): Using Xvfb for GUI unit tests so they can run as an automated task. This was broken by r2602

--- src/gui/unit_tests/unit_tests.sh
+++ src/gui/unit_tests/unit_tests.sh
@@ -2,10 +2,16 @@
 
 commands=$@
 
+Xvfb :1 &
+PID=$!
+export DISPLAY=":1"
+
 for directory in `find . -maxdepth 1 -type d -regex '\./[A-Za-z0-9\-\_]*'`
 do
     cd $directory
     [ ! -e Makefile ] && qmake -spec $QMAKESPEC
     $commands
     cd -
-done;
+done
+
+kill $PID
