6aa670d538dc1c93105f46e91e3cdfdaf8b26f37
Vadim Kurland <vadim@netcitadel.com>
2010-04-19 17:22:07 +0000

    start and stop Xvfb for each unit test separately in an attempt to fight its constant crashes on Ubuntu intrepid

--- src/gui/unit_tests/unit_tests.sh
+++ src/gui/unit_tests/unit_tests.sh
@@ -2,16 +2,18 @@
 
 commands=$@
 
-Xvfb :1 &
-PID=$!
-export DISPLAY=":1"
 
 for directory in `find . -maxdepth 1 -type d -regex '\./[A-Za-z0-9\-\_]*'`
 do
     cd $directory
     [ ! -e Makefile ] && qmake -spec $QMAKESPEC
+
+    Xvfb :1 &
+    PID=$!
+    export DISPLAY=":1"
     $commands || { kill $PID; exit 1; }
+    kill $PID || exit 0
+
     cd -
 done
 
-kill $PID || exit 0
