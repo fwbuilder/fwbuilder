c88c5d726baf5ccd43bda1e2725c5b611db37990
Vadim Kurland <vadim@netcitadel.com>
2010-04-19 19:47:31 +0000

    now run-tests.py deals with Xvfb instead of scripts inside fwbuilder module

--- src/gui/unit_tests/unit_tests.sh
+++ src/gui/unit_tests/unit_tests.sh
@@ -2,22 +2,11 @@
 
 commands=$@
 
-
 for directory in `find . -maxdepth 1 -type d -regex '\./[A-Za-z0-9\-\_]*'`
 do
     cd $directory
     [ ! -e Makefile ] && qmake -spec $QMAKESPEC
-
-    PID=""
-    [ -z "$DISPLAY" ] && {
-        Xvfb :1 &
-        PID=$!
-        export DISPLAY=":1"
-    }
-    $commands; TEST_STATUS=$?
-    [ -n "$PID" ] && kill $PID
-    [ $TEST_STATUS ] || exit 0
-
+    $commands || exit 1
     cd -
 done
 
--- src/iptlib/unit_tests/unit_tests.sh
+++ src/iptlib/unit_tests/unit_tests.sh
@@ -2,16 +2,11 @@
 
 commands=$@
 
-Xvfb :1 &
-PID=$!
-export DISPLAY=":1"
-
 for directory in `find . -maxdepth 1 -type d -regex '\./[A-Za-z0-9\-\_]*'`
 do
     cd $directory
     [ ! -e Makefile ] && qmake -spec $QMAKESPEC
-    $commands || { kill $PID; exit 1; }
+    $commands || exit 1;
     cd -
 done
 
-kill $PID || exit 0
