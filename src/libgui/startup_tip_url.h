746f7c89ef470aad0941f86521e0864777e5d65f
Sirius Bakke <sirius@bakke.co>
2017-01-13 21:02:00 +0100

    fix: Add C++11 compatability

--- src/libgui/startup_tip_url.h
+++ src/libgui/startup_tip_url.h
@@ -1,4 +1,4 @@
-/* 
+/*
 
                           Firewall Builder
 
@@ -17,7 +17,7 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
- 
+
   To get a copy of the GNU General Public License, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
@@ -29,7 +29,7 @@
 
 #include "../../VERSION.h"
 
-#define STARTUP_TIP_URL "http://update.fwbuilder.org/tips/gettip.cgi?v="VERSION
+#define STARTUP_TIP_URL "http://update.fwbuilder.org/tips/gettip.cgi?v=" VERSION
 
 #endif
 
