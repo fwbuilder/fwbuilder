9edc675d4b19e9c8eb78b4c86dbd253791309d87
Sirius Bakke <sirius@bakke.co>
2018-04-30 15:52:01 +0200

    chore: Added build script for mxe-w32, travis

--- build_mxe-w32.sh
+++ build_mxe-w32.sh
@@ -1,14 +1,80 @@
 #!/bin/bash
 JOBS=$(nproc --all)
-
 export PATH=/usr/lib/mxe/usr/bin:$PATH
 export TOOLCHAIN_PREFIX=/usr/lib/mxe/usr/i686-w64-mingw32.shared
+export CXXFLAGS="-Wno-maybe-uninitialized -Wno-attributes"
 
-./autogen.sh \
-  --with-xml2-config=/usr/lib/mxe/usr/i686-w64-mingw32.shared/bin/xml2-config \
-  --with-xslt-config=/usr/lib/mxe/usr/i686-w64-mingw32.shared/bin/xslt-config \
-  --with-qtdir=/usr/lib/mxe/usr/i686-w64-mingw32.shared/qt5 \
-  --host=i686-w64-mingw32.shared
-make -j${JOBS}
+main()
+{
+    if [ $# -eq 0 ]; then
+        usage
+        exit
+    fi
 
-makensis -nocd packaging/fwbuilder.nsi
+    while [ "$1" != "" ]; do
+        case $1 in
+        -h | --help | help | usage )
+            usage
+            exit
+            ;;
+        all )
+            configure
+            compile
+            package
+            exit
+            ;;
+        configure )
+            configure
+            ;;
+        compile )
+            compile
+            ;;
+        package )
+            package
+            ;;
+        * )
+            usage
+            exit 1
+            ;;
+        esac
+        shift
+    done
+}
+
+usage()
+{
+    echo "Usage: $0 [ all | configure | compile | package ]"
+}
+
+configure()
+{
+    echo "==> Configuring"
+    ./autogen.sh \
+      --with-xml2-config=/usr/lib/mxe/usr/i686-w64-mingw32.shared/bin/xml2-config \
+      --with-xslt-config=/usr/lib/mxe/usr/i686-w64-mingw32.shared/bin/xslt-config \
+      --with-qtdir=/usr/lib/mxe/usr/i686-w64-mingw32.shared/qt5 \
+      --host=i686-w64-mingw32.shared
+    if [ $? -eq 0 ]; then
+        echo "==> Done configuring"
+    fi
+}
+
+compile()
+{
+    echo "==> Compiling"
+    make -j${JOBS}
+    if [ $? -eq 0 ]; then
+        echo "==> Done compiling"
+    fi
+}
+
+package()
+{
+    echo "==> Packaging"
+    makensis -nocd packaging/fwbuilder.nsi
+    if [ $? -eq 0 ]; then
+        echo "==> Done packaging"
+    fi
+}
+
+main "$@"
---
+++ docker/travis.mxe-w32-ubuntu:18.04.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e -x
+
+./build_mxe-w32.sh configure compile
